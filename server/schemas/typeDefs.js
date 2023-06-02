42f11e82d8f761ef2825132e170ec67de052d568
Leah <leahxharden@gmail.com>
2023-06-02 18:55:00 -0400

    Update typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,10 +39,12 @@ type Auth {
 type Mutation {
     createUser(username: String!, email: String!, password: String!): Auth
     saveBook(book: BookInput!): User
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+
+
+
 }`
 
-// login: Accepts an email and password as parameters; returns an Auth type.
-
-//     addUser: Accepts a username, email, and password as parameters; returns an Auth type.
 
 module.exports = typeDefs;
